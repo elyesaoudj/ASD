8cc6583ffd57d3b56072b0feaa747ef52c3c1243
elyesaoudj <150586689+elyesaoudj@users.noreply.github.com>
2024-11-28 11:09:22 +0100

    Add files via upload

---
+++ list/libraries/list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+Node* createNode(int value)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void deleteNode(Node* node)
+{
+    free(node);
+}
+
+int getValue(Node* node)
+{
+    return node ? node->value : -1; // Return -1 if node is NULL
+}
+
+void setValue(Node* node, int value)
+{
+    if (node)
+    {
+        node->value = value;
+    }
+}
+
+Node* getNext(Node* node)
+{
+    return node ? node->next : NULL;
+}
+
+void setNext(Node* node, Node* nextNode)
+{
+    if (node)
+    {
+        node->next = nextNode;
+    }
+}
